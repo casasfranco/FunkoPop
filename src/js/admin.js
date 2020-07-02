d5a9c0b0533752aeb48c98b387a71c9a52ab721b
Franco Casas <casassfranco@gmail.com>
2020-07-02 10:12:17 -0300

    se creo la clase funko

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,2 +1,8 @@
-let saludo = "hola desde admin";
-console.log(saludo);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '../css/style.css'
+
+//Class
+import Funko from '../js/funko.js';
+
+let funko  = new Funko();
---
+++ src/js/funko.js
@@ -0,0 +1,17 @@
+//Clase funko 
+
+export default class Funko {
+    constructor(codigo, nombre, mumSerie, descripcion, imagen, precio, categoria){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.numSerie = numSerie;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.destacado = false;
+        this.estado = 1;
+    }
+
+    //Methods
+}
